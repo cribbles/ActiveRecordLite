1e38b29bfea9c9d0e7607fc1cd53bb30c20b942a
cribbles <yaphetf@gmail.com>
2015-08-30 09:50:30 -0400

    fix syntax error in Associatable

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -1,4 +1,4 @@
-require_relative 'searchable'
+require_relative 'sql_relation'
 require 'active_support/inflector'
 
 class AssocOptions
@@ -78,36 +78,36 @@ module Associatable
 
       model.where(where_params)
     end
+  end
 
-    def has_one_through(name, through_name, source_name)
-      through_options = assoc_options[through_name]
+  def has_one_through(name, through_name, source_name)
+    through_options = assoc_options[through_name]
 
-      define_method(name) do
-        source_options = through_options.model_class.assoc_options[source_name]
+    define_method(name) do
+      source_options = through_options.model_class.assoc_options[source_name]
 
-        through = through_options.model_class.table_name
-        source = source_options.model_class.table_name
+      through = through_options.model_class.table_name
+      source = source_options.model_class.table_name
 
-        through_foreign = through_options.foreign_key
-        through_primary = through_options.primary_key
-        source_foreign = source_options.foreign_key
-        source_primary = source_options.primary_key
+      through_foreign = through_options.foreign_key
+      through_primary = through_options.primary_key
+      source_foreign = source_options.foreign_key
+      source_primary = source_options.primary_key
 
-        result = DBConnection.execute(<<-SQL, send(through_foreign)).first
-            SELECT
-              #{source}.*
-            FROM
-              #{source}
-            JOIN
-              #{through}
-            ON
-              #{through}.#{source_foreign} = #{source}.#{source_primary}
-            WHERE
-              #{through}.#{through_primary} = ?
-          SQL
+      result = DBConnection.execute(<<-SQL, send(through_foreign)).first
+        SELECT
+          #{source}.*
+        FROM
+          #{source}
+        JOIN
+          #{through}
+        ON
+          #{through}.#{source_foreign} = #{source}.#{source_primary}
+        WHERE
+          #{through}.#{through_primary} = ?
+      SQL
 
-          source_options.model_class.new(result)
-      end
+      source_options.model_class.new(result)
     end
   end
 end
